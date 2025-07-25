becaa22346ff88fc43a08e573f8cc118adf3da05
q960913 <87472382+q960913@users.noreply.github.com>
2025-07-25 14:07:37 +0800

    Initial commit

---
+++ src/components/BingQueenAi/Git/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BingQueenAiGit',
+  data() {
+    return {
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
