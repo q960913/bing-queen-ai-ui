1bbf69f561542e9075e5bf5202a45a67db87ec20
bingqueen <bing.queen@outlook.com>
2025-07-28 19:05:35 +0800

    feat；前端功能新增和优化

--- src/store/modules/chat.js
+++ src/store/modules/chat.js
@@ -20,7 +20,7 @@ const state = {
     }, {
       id: 2,
       role: 'user',
-      content: '我想把它部署到 Vercel。',
+      content: '我想`把`它部署`到` Vercel。',
       avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
       turnId: 'turn-uuid-123', // [新增] 轮次ID
       activeVersion: 2,         // [新增] 当前版本号
